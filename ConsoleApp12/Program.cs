5a6c5aeaa0d4df19606608c9e2197fedbf05fd9e
r0zmarin1 <irozina2006@gmail.com>
2023-10-16 03:29:56 +1000

    add task 2

--- ConsoleApp12/Program.cs
+++ ConsoleApp12/Program.cs
@@ -2,6 +2,7 @@
 {
     class Program
     {
+        /*
         public static void Main(string[] args) 
         { 
             // task 1
@@ -18,6 +19,23 @@
                 return 0;
             int digit = number % 10;
             return digit + CalcDigitSum(number / 10); // recurs
+        }
+        */
+
+        // task 2
+        public static void Main(string[] args) 
+        {
+            int.TryParse(Console.ReadLine(), out int number);
+
+            int result = CalcNum(number);
+            Console.WriteLine(result);
+        }
+
+        static int CalcNum(int number)
+        {
+            if (number == 0)
+                return 0;
+            return 1 + CalcNum(number / 10);
         }
     }
 }
