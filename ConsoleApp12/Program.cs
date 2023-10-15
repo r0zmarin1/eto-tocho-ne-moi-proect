556207bd2b7f8e59f318505299098ec38807c06f
r0zmarin1 <irozina2006@gmail.com>
2023-10-16 02:59:24 +1000

    task 1

--- ConsoleApp12/Program.cs
+++ ConsoleApp12/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿namespace eto_ne_proect
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        { 
+            // task 1
+
+            int.TryParse(Console.ReadLine(), out int number);
+
+            int result = CalcDigitSum(number);
+            Console.WriteLine(result);
+        }
+
+        static int CalcDigitSum(int number)
+        {
+            if (number == 0)
+                return 0;
+            int digit = number % 10;
+            return digit + CalcDigitSum(number / 10); // recurs
+        }
+    }
+}
