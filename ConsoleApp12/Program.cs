08f96be093ba90bf62e6e68451c97a2e25466837
r0zmarin1 <irozina2006@gmail.com>
2023-10-16 23:47:22 +1000

    new

--- ConsoleApp12/Program.cs
+++ ConsoleApp12/Program.cs
@@ -23,6 +23,8 @@
         */
 
         // task 2
+
+        /*
         public static void Main(string[] args) 
         {
             int.TryParse(Console.ReadLine(), out int number);
@@ -37,5 +39,65 @@
                 return 0;
             return 1 + CalcNum(number / 10);
         }
-    }
+        */
+
+        //task 3
+        /*
+        public static void Main(string[] args)
+        {
+            int.TryParse(Console.ReadLine(), out int m);
+            int.TryParse(Console.ReadLine(), out int n);
+
+            int result = kolvo(m, n);
+            Console.WriteLine(result);
+        }
+
+        static int kolvo(int m, int n)
+        {
+            if (n == 0 || m == 0)
+                return 1;
+            return kolvo(m, n - 1) + kolvo(m - 1, n - 1);
+        }
+        */
+
+        // task 4
+
+        /*
+        public static void Main(string[] args)
+        {
+            double.TryParse(Console.ReadLine(), out double xn);
+            double.TryParse(Console.ReadLine(), out double xk);
+            double.TryParse(Console.ReadLine(), out double epsy);
+
+            int result = reshenie();
+        }
+        static int reshenie(double xk, double xn, double epsy)
+        {
+            if (xk - xn > epsy)
+            {
+
+                double dx = (xk - xn) / 2;
+                double xi = xn + dx;
+                if (xn < 0 && xi > 0 || xn > 0 && xi < 0)
+                {
+                    xk = xi;
+                }
+                else
+                    xn = xi;
+                reshenie(xk, xn, epsy);
+                return xi;
+                
+            }
+            return 1;
+        }    
+        */
+
+        // task 6
+        public static void Main(string[] args)
+        {
+            double.TryParse(Console.ReadLine(), out double i);
+            double.TryParse(Console.ReadLine(), out double j);
+
+        }
+    }   
 }
